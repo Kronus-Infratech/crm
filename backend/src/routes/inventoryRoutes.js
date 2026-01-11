2c1d5272e7b820beb97af8a5e5457ff8a80517cc
nakul <jaglan.nakul@gmail.com>
2026-01-11 18:43:03 +0530

    Changed Access of inventory routes

--- backend/src/routes/inventoryRoutes.js
+++ backend/src/routes/inventoryRoutes.js
@@ -11,26 +11,25 @@ const {
     deleteProject,
     getInventoryItemById
 } = require('../controllers/inventoryController');
-const { protect, authorize } = require('../middleware/auth');
-const { ROLES } = require('../config/constants');
+const { protect } = require('../middleware/auth');
 
 // Project Routes
 router.route('/projects')
     .get(protect, getProjects)
-    .post(protect, authorize(ROLES.ADMIN, ROLES.MANAGER, ROLES.DIRECTOR, ROLES.EXECUTIVE), createProject);
+    .post(protect, createProject);
 
 router.route('/projects/:id')
-    .put(protect, authorize(ROLES.ADMIN, ROLES.MANAGER, ROLES.DIRECTOR, ROLES.EXECUTIVE), updateProject)
-    .delete(protect, authorize(ROLES.ADMIN, ROLES.DIRECTOR), deleteProject);
+    .put(protect, updateProject)
+    .delete(protect, deleteProject);
 
 // Inventory Item Routes
 router.route('/items')
     .get(protect, getInventoryItems)
-    .post(protect, authorize(ROLES.ADMIN, ROLES.MANAGER, ROLES.DIRECTOR, ROLES.EXECUTIVE), createInventoryItem);
+    .post(protect, createInventoryItem);
 
 router.route('/items/:id')
     .get(protect, getInventoryItemById)
-    .put(protect, authorize(ROLES.ADMIN, ROLES.MANAGER, ROLES.DIRECTOR, ROLES.EXECUTIVE), updateInventoryItem)
-    .delete(protect, authorize(ROLES.ADMIN, ROLES.DIRECTOR), deleteInventoryItem);
+    .put(protect, updateInventoryItem)
+    .delete(protect, deleteInventoryItem);
 
 module.exports = router;
